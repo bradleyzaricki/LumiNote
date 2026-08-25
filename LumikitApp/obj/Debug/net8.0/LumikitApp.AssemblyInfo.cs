cf9982eba5683ce04ce13ed841cfd5bc0c1f1cfe
bzaricki <bzaricki56@gmail.com>
2026-08-25 01:44:17 -0400

    chore: checkpoint build artifacts and add IDE/Junie project files

--- LumikitApp/obj/Debug/net8.0/LumikitApp.AssemblyInfo.cs
+++ LumikitApp/obj/Debug/net8.0/LumikitApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LumikitApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62eb8b9ff751c7d3a171b4deda64759617f14e81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02f041462ecae6876aa1245c12722d959d620f91")]
 [assembly: System.Reflection.AssemblyProductAttribute("LumikitApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LumikitApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
